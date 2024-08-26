40efda2d0b7518648413dab2161a946267d6c2ca
Pranjal Parihar <84609625+pranjal54321@users.noreply.github.com>
2024-08-26 09:39:56 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({ base: "/{Netflix_Colne}", plugins: [react()], }) 
+export default defineConfig({
+    plugins: [react()],
+    base: "/Netflix-app/"
+  })
+  
